a7e92533f4427cca20dbfdde9589ae92e3f0463b
Виталий Шевцов <vshevtsov17@gmail.com>
2024-12-25 21:02:41 +0200

    logs models & migrations

--- back/logs/admin.py
+++ back/logs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+from logs.models import Log
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "level",
+        "display_text",
+        "created",
+    )
+    list_filter = ("level",)
+
+    def display_text(self, obj):
+        return f"{obj.text[:50]}..."
+
+    display_text.short_description = _("Text")
--- back/logs/models.py
+++ back/logs/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from asgiref.sync import sync_to_async
+
+
+class Log(models.Model):
+    class LogLevel(models.TextChoices):
+        INFO = "info", "Info"
+        WARNING = "warning", "Warning"
+        ERROR = "error", "Error"
+
+    level = models.SlugField(
+        choices=LogLevel.choices, default=LogLevel.INFO, verbose_name=_("Log Level")
+    )
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created"))
+
+    def __str__(self) -> str:
+        return self.level
+
+    @staticmethod
+    def info(text: str):
+        return Log.objects.create(level=Log.LogLevel.INFO, text=text)
+
+    @staticmethod
+    def warning(text: str):
+        print("WARNING: ", text)
+        return Log.objects.create(level=Log.LogLevel.WARNING, text=text)
+
+    @staticmethod
+    def error(text: str):
+        print("ERROR: ", text)
+        return Log.objects.create(level=Log.LogLevel.ERROR, text=text)
+
+    @staticmethod
+    @sync_to_async
+    def ainfo(text: str):
+        return Log.info(text)
+
+    @staticmethod
+    @sync_to_async
+    def awarning(text: str):
+        return Log.warning(text)
+
+    @staticmethod
+    @sync_to_async
+    def aerror(text: str):
+        return Log.error(text)
+
+    class Meta:
+        verbose_name = _("Log")
+        verbose_name_plural = _("Logs")
