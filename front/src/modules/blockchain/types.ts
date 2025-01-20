3993a299f6f972b6601ee6e2eaa5eefe22adb5c8
Виталий Шевцов <vshevtsov17@gmail.com>
2025-01-20 18:49:31 +0200

    front blockchain enums & types

---
+++ front/src/modules/blockchain/types.ts
@@ -0,0 +1,14 @@
+import { EBlockchainValidatorStatus } from "@modules/blockchain/enums";
+
+export type TBlockchainValidator = {
+  id: number;
+  blockchain_id: number;
+  operator_address: string;
+  moniker?: string;
+  identity?: string;
+  voting_power: number;
+  commision_rate: number;
+  uptime: number;
+  status: EBlockchainValidatorStatus;
+  updated: Date;
+};
