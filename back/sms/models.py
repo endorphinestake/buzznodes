cec323e79fcf650e471b9a4cef2f58b3d04309fe
Виталий Шевцов <vshevtsov17@gmail.com>
2025-02-01 01:29:36 +0200

    sms migrations

--- back/sms/models.py
+++ back/sms/models.py
@@ -11,7 +11,7 @@ from alerts.models import (
 )
 
 
-class SMSAlert(models.Model):
+class SMSBase(models.Model):
     class Status(models.TextChoices):
         SENT = "SENT", _("Sent")
         DELIVERED = "DELIVERED", _("Delivered")
@@ -19,9 +19,7 @@ class SMSAlert(models.Model):
         REJECTED = "REJECTED", _("Rejected")
         ERROR = "ERROR", _("Error")
 
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="user_sms", verbose_name=_("User")
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("User"))
     phone = models.ForeignKey(
         UserPhone,
         on_delete=models.SET_NULL,
@@ -29,6 +27,27 @@ class SMSAlert(models.Model):
         blank=True,
         verbose_name=_("Phone Number"),
     )
+    sent_text = models.TextField(verbose_name=_("SMS Text"))
+    status = models.SlugField(
+        choices=Status.choices,
+        max_length=25,
+        default=Status.SENT,
+        verbose_name=_("Status"),
+    )
+    err = models.CharField(
+        max_length=256, null=True, blank=True, verbose_name=_("Error Details")
+    )
+    updated = models.DateTimeField(auto_now=True, verbose_name=_("Updated"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created"))
+
+    def __str__(self):
+        return f"{self.phone} -> {self.sent_text[:50]}"
+
+    class Meta:
+        abstract = True
+
+
+class SMSAlert(SMSBase):
     user_alert_setting_voting_power = models.ForeignKey(
         UserAlertSettingVotingPower,
         on_delete=models.SET_NULL,
@@ -69,34 +88,13 @@ class SMSAlert(models.Model):
         related_name="user_alert_setting_tombstoned_status_sms",
         verbose_name=_("User Alert Setting Tombstoned Status"),
     )
-    sent_text = models.TextField(verbose_name=_("SMS Text"))
-    status = models.SlugField(
-        choices=Status.choices,
-        max_length=25,
-        default=Status.SENT,
-        verbose_name=_("Status"),
-    )
-    err = models.CharField(
-        max_length=256, null=True, blank=True, verbose_name=_("Error Details")
-    )
-    updated = models.DateTimeField(auto_now=True, verbose_name=_("Updated"))
-    created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created"))
-
-    def __str__(self):
-        return f"{self.phone} -> {self.sent_text}"
 
     class Meta:
         verbose_name = _("SMS Alert")
         verbose_name_plural = _("SMS Alerts")
 
 
-class SMSConfirm(SMSAlert):
-    user_alert_setting_voting_power = None
-    user_alert_setting_uptime = None
-    user_alert_setting_comission = None
-    user_alert_setting_jailed_status = None
-    user_alert_setting_tombstoned_status = None
-
+class SMSConfirm(SMSBase):
     code = models.CharField(
         max_length=25,
         blank=True,
