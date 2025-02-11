b04d0e07aa6c917c57b557798fe5a2a2aea0560e
Виталий Шевцов <vshevtsov17@gmail.com>
2025-02-11 21:46:15 +0200

    added WebhookSMSBirdView

--- back/sms/views.py
+++ back/sms/views.py
@@ -42,6 +42,27 @@ class WebhookSMSBirdView(views.APIView):
 
     def post(self, request):
         print("WebhookSMSBirdView POST:", request.POST)
-        print("WebhookHicellView DATA:", request.data)
+        print("WebhookSMSBirdView DATA:", request.data)
+
+        sms_id = request.data.get("payload", {}).get("id", None)
+        sms_status = request.data.get("payload", {}).get("status", None)
+        if not sms_id:
+            return response.Response(
+                "Message ID not found!", status=status.HTTP_400_BAD_REQUEST
+            )
+
+        sms_instance = find_sms_by_id(sms_id)
+        if sms_instance:
+            statuses = {
+                "delivered": SMSBase.Status.DELIVERED,
+                "accepted": SMSBase.Status.SENT,
+                "processing": SMSBase.Status.SENT,
+                "sent": SMSBase.Status.SENT,
+                "sending_failed": SMSBase.Status.REJECTED,
+                "delivery_failed": SMSBase.Status.UNDELIVRED,
+            }
+
+            sms_instance.status = statuses.get(sms_status, SMSBase.Status.ERROR)
+            sms_instance.save()
 
         return response.Response("OK", status=status.HTTP_200_OK)
